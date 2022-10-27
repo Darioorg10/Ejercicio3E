01a94e883af1ef237121bf170a5cd83b7f5ddc9f
Darioorg10 <dricgar1103@g.educaand.es>
2022-10-27 19:28:03 +0200

    Ejercicio 3E empezado

--- src/main/java/dario/Ejercicio3E_DarioRicoGarcia.java
+++ src/main/java/dario/Ejercicio3E_DarioRicoGarcia.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package dario;
 
 import javax.swing.JOptionPane;
@@ -14,38 +13,39 @@ import javax.swing.JOptionPane;
 public class Ejercicio3E_DarioRicoGarcia {
 
     public static void main(String[] args) {
-        
-        
-        
-        // Creamos el primer bucle, donde si la opcion es diferente a salir
+
+        // Creamos el primer bucle, donde si la opcion es diferente a calcular o terminar
         // se vuelve a pedir
         String opcion;
-        do {
+        String codigo;
+        do { // Bucle del menú inicial
 
             // Creamos el menú de las opciones
-        
-        String opcionTexto = """
+            String opcionTexto = """
                           ----------------------------------------------------------------
                                                     MENÚ
                           ----------------------------------------------------------------
                           Para realizar los cálculos: calcular
                           Para terminar el programa: terminar
                           ----------------------------------------------------------------""";
-                          
-        opcion = JOptionPane.showInputDialog(opcionTexto);
-        
-        if (opcion.equals("calcular"))
+
+            opcion = JOptionPane.showInputDialog(opcionTexto);
+
+            if (opcion.equalsIgnoreCase("calcular")){
             
-        String codigo = JOptionPane.showInputDialog("Introduce el código "
-                + "del dulce").toUpperCase();
+                do {
+                    String opcionCodigo = """
+                                          Di el código del producto""";
+                    codigo = JOptionPane.showInputDialog(opcionCodigo);
+                    System.out.println("cálculos");
+                } while (!(codigo.equalsIgnoreCase("M1")));
             
-        } while (opcion.equalsIgnoreCase("salir"));
             
             
             
-        }
-        
-        
-        
+            }
+
+        } while (!opcion.equalsIgnoreCase("terminar"));
+
     }
 }
